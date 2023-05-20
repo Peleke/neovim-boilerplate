2e556d891bf7649a7e387e79979995ca03451148
Peleke <kraken@peleke.33mail.com>
2023-05-20 16:54:51 -0400

    Add some options to init.lua.

--- init.lua
+++ init.lua
@@ -10,6 +10,36 @@ require "lazy-bootstrap"
 require "settings"
 require "keybindings"
 
+-- Incremental live completion (note: this is now a default on master).
+vim.o.inccommand = "nosplit"
+
+-- Set highlight on search. This will remove the highlight after searching for text.
+vim.o.hlsearch = false
+
+-- Make relative line numbers default. The current line number will be shown as well as relative numbering from that current line. It makes navigating around code easier.
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Do not save when switching buffers (note: default on master).
+vim.o.hidden = true
+
+-- Enable mouse mode. Sometimes it"s easier to scroll using the touchpad for example.
+vim.o.mouse = "a"
+
+-- Enable break indent.
+vim.o.breakindent = true
+
+-- Save undo history.
+vim.opt.undofile = true
+
+-- Case insensitive searching unless /C or capital in search.
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time.
+vim.o.updatetime = 250
+vim.wo.signcolumn = "yes"
+
 -- Plugin management {{{
 local lazy = require("lazy")
 lazy.setup("plugins")
